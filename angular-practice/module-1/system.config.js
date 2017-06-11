d2a6ec0ea0c07b338365fe116c1a3dc0207a5193
Ankit Rana <ankit.rana.1195@gmail.com>
2017-06-11 13:34:24 +0530

    Change the app configuration

--- angular-practice/module-1/system.config.js
+++ angular-practice/module-1/system.config.js
@@ -12,7 +12,6 @@
         map: {
             // our app is within the app folder
             'app': 'app',
-
             // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -21,8 +20,6 @@
             '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
             '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-            '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-
             // other libraries
             'rxjs': 'npm:rxjs'
         },
